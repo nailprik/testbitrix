4fffb14a57459bd7b4bec834c88c13f09eb2d0f8
nail galiev <60182436+nailprik@users.noreply.github.com>
2021-03-29 17:25:47 +0300

    Update settings.py

--- duplicates/settings.py
+++ duplicates/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_!@9osea6)7b23=58e1wsw5r9rdzthq$hd8n0x3w5tx8ybpqgz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
